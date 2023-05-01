84d32b05f4af6c0743ce85dd13a5ef63046933b2
Vasu Dutt <2vasudutt@gmail.com>
2023-05-02 02:19:21 +0530

    Fix: Link logo to homepage

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 const Logo = () => {
+  const router = useRouter();
+
   return (
     <Image
       alt="logo"
       className="hidden cursor-pointer md:block"
+      onClick={() => router.push('/')}
       height={100}
       width={100}
       src={'/images/logo.png'}
