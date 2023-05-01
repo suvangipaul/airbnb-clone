c3f1f8eaebf8abf3ea6d755d993aedd31317fbd8
Vasu Dutt <2vasudutt@gmail.com>
2023-05-02 00:22:21 +0530

    Fix home layout grid

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ const Home = async ({ searchParams }: HomeProps) => {
 
   return (
     <Container>
-      <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        <div>
+      <div>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {listings.map((listing: any) => (
             <ListingCard
               currentUser={currentUser}
